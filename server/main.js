5d3426a14ece3b76297b5c81d575cd4106cc4e3a
Marcus Francis <marcusmagnusson@outlook.com>
2019-01-27 09:33:49 +0100

    Mongo collection
    & fake data with faker

--- server/main.js
+++ server/main.js
@@ -1,5 +1,25 @@
-import { Meteor } from 'meteor/meteor';
+import { Meteor } from "meteor/meteor";
+import { Info } from "../imports/collections/collection";
+import { image, helpers } from "faker";
 
 Meteor.startup(() => {
-  // code to run on server at startup
+
+  // Generate faker data if empty
+  const numberRecords = Info.find({}).count();
+ 
+  if (!numberRecords) {
+    var times = 100;
+    for (var i = 0; 1 < times; i++) {
+      const { name, email, phone, address } = helpers.createCard();
+      Info.insert({
+        name,
+        email,
+        phone,
+        address,
+        avatar: image.avatar()
+      });
+    }
+  }
+console.log(numberRecords);
+  
 });
